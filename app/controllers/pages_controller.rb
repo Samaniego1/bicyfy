524a01a54130524ffaa2c0027d961e6f865f4066
Stefano Mandolesi <stefano232@gmail.com>
2022-02-25 12:41:05 -0300

    Added searchbar and fixed some front end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @bikes = Bike.all
+    if params[:query].present?
+      @bikes = Bike.search_by_bike(params[:query])
+    else
+      # render: "home"
+      @bikes = Bike.all
+    end
   end
 end
